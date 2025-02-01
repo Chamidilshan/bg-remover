28b3312fcf6c5bce6211bbf91cfe8c377eef1424
Chamika Dilshan <chamidilshan55@gmail.com>
2025-02-01 23:50:39 +0530

    created folder structure and added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [NavbarComponent]
 })
 export class AppComponent {
   title = 'bg-remover';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './features/about/about.component';
+import { PricingComponent } from './features/pricing/pricing.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'pricing', component: PricingComponent},
+    {path: 'contact', component: ContactComponent}
+];
